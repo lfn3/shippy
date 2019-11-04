6db726bf8f78932ec1190d02c337d043921fbd31
Liam Falconer <liam@lfn3.net>
2019-11-04 07:27:44 +0000

    Proper arg parsing

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,33 @@ use shippy::git_lab::Project;
 use shippy::{print_release_notes, Config};
 use std::env;
 use std::fs::File;
+use structopt::StructOpt;
+use std::path::PathBuf;
 
 extern crate shippy;
 
+#[derive(StructOpt)]
+#[structopt(name = "shippy", about = "Release note generator")]
+struct Opts {
+    #[structopt(index = 1)]
+    tag_prefix: String,
+
+    #[structopt(index = 2, default_value = "HEAD")]
+    up_to: String,
+
+    #[structopt(short = "c", long = "config_file", parse(from_os_str), default_value = "./shippy.yml")]
+    config_file: PathBuf,
+
+    #[structopt(short = "t", long = "team")]
+    team: Option<String>
+}
+
 fn main() -> Result<(), CliError<'static>> {
     let cwd = env::current_dir().map_err(|e| CliError::Io("Could not get current_dir", e))?;
-    let args: Vec<String> = env::args().collect();
+    let opts = Opts::from_args();
 
-    if args.len() < 2 {
-        return Err(CliError::Str(
-            "Expected at least one argument containing a tag prefix",
-        ));
-    }
-
-    let mut cfg_path = cwd.clone();
-    cfg_path.push("shippy.yml");
     let cfg_file =
-        File::open(cfg_path).map_err(|e| CliError::Io("Could not open config file", e))?;
+        File::open(opts.config_file).map_err(|e| CliError::Io("Could not open config file", e))?;
 
     let cfg: Config = serde_yaml::from_reader(cfg_file)
         .map_err(|e| CliError::Yaml("Could not deserialize config file", e))?;
@@ -32,7 +42,7 @@ fn main() -> Result<(), CliError<'static>> {
     print_release_notes(
         &proj,
         repo,
-        args[1].as_str(),
-        args.get(2).map_or_else(|| "HEAD", |s| s.as_str()),
+        opts.tag_prefix.as_str(),
+        opts.up_to.as_str(),
     )
 }
